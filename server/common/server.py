666d17fadfbde64f9165cb008bca4a72ba1dffa0
ricomateo <mrico@fi.uba.ar>
2025-03-22 18:40:31 -0300

    add server gracefully shutdown

--- server/common/server.py
+++ server/common/server.py
@@ -1,13 +1,16 @@
 import socket
 import logging
-
+import signal
+import sys
 
 class Server:
     def __init__(self, port, listen_backlog):
         # Initialize server socket
+        signal.signal(signal.SIGTERM, self.__exit_gracefully)
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self._client_sock = None
 
     def run(self):
         """
@@ -21,10 +24,10 @@ class Server:
         # TODO: Modify this program to handle signal to graceful shutdown
         # the server
         while True:
-            client_sock = self.__accept_new_connection()
-            self.__handle_client_connection(client_sock)
+            self._client_sock = self.__accept_new_connection()
+            self.__handle_client_connection()
 
-    def __handle_client_connection(self, client_sock):
+    def __handle_client_connection(self):
         """
         Read message from a specific client socket and closes the socket
 
@@ -33,15 +36,15 @@ class Server:
         """
         try:
             # TODO: Modify the receive to avoid short-reads
-            msg = client_sock.recv(1024).rstrip().decode('utf-8')
-            addr = client_sock.getpeername()
+            msg = self._client_sock.recv(1024).rstrip().decode('utf-8')
+            addr = self._client_sock.getpeername()
             logging.info(f'action: receive_message | result: success | ip: {addr[0]} | msg: {msg}')
             # TODO: Modify the send to avoid short-writes
-            client_sock.send("{}\n".format(msg).encode('utf-8'))
+            self._client_sock.send("{}\n".format(msg).encode('utf-8'))
         except OSError as e:
             logging.error("action: receive_message | result: fail | error: {e}")
         finally:
-            client_sock.close()
+            self._client_sock.close()
 
     def __accept_new_connection(self):
         """
@@ -56,3 +59,10 @@ class Server:
         c, addr = self._server_socket.accept()
         logging.info(f'action: accept_connections | result: success | ip: {addr[0]}')
         return c
+
+    def __exit_gracefully(self, signum, frame):
+        logging.info("Received SIGTERM signal, exiting gracefully")
+        self._server_socket.shutdown(socket.SHUT_RDWR)
+        if self._client_sock is not None:
+            self._client_sock.close()
+        sys.exit(0)
