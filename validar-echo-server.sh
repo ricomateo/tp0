ea5dfe4b61a2f8665d12d6861a46061a92972849
ricomateo <mrico@fi.uba.ar>
2025-03-22 13:58:17 -0300

    supress build output in validar-echo-server.sh

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,10 +1,12 @@
-docker build . -f netcat.Dockerfile -t nc
+# Build the netcat image, capturing its output
+BUILD_OUTPUT=$(docker build . -q -f netcat.Dockerfile -t nc)
 
 MESSAGE="hello"
 HOST=server
 PORT=12345
+NETWORK=tp0_testing_net
 
-OUTPUT=$(docker run -it --rm --network=tp0_testing_net --name=netcat nc -c "echo $MESSAGE | nc $HOST $PORT" | tr -d '\r')
+OUTPUT=$(docker run -q -it --rm --network=$NETWORK --name=netcat nc -c "echo $MESSAGE | nc $HOST $PORT" | tr -d '\r')
 
 if [[ "$OUTPUT" == "$MESSAGE" ]]; then
     echo "action: test_echo_server | result: success"
