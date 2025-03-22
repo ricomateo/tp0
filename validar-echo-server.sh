51b7a435c5bab647079b43f918599fe487f90bf9
ricomateo <mrico@fi.uba.ar>
2025-03-22 14:31:39 -0300

    change validar-echo-server.sh so it works both with success and failure

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -7,7 +7,7 @@ PORT=12345
 NETWORK=tp0_testing_net
 
 # Start the container, send "hello" to the server, and save the response in 'output.txt'
-docker run -q -d --rm --network=$NETWORK --name=netcat nc -c "echo $MESSAGE | nc $HOST $PORT" > output.txt 2> error.txt
+RESPONSE=$(docker run --rm --network=$NETWORK --name=netcat nc -c "echo $MESSAGE | nc $HOST $PORT" > output.txt 2> error.txt)
 
 RESPONSE=$(cat output.txt | tr -d '\r')
 rm output.txt
