7cd087400241eea4b93287c438a59d14d32f12a8
ricomateo <mrico@fi.uba.ar>
2025-03-25 20:21:20 -0300

    add missing log

--- server/common/server.py
+++ server/common/server.py
@@ -1,7 +1,7 @@
 import logging
 from common.communication import CommunicationHandler
 from common.error import MessageReceptionError
-from common.utils import store_bets
+from common.utils import store_bets, Bet
 
 SOCKET_TIMEOUT = 5
 BET_INFO_MSG_TYPE = 0
@@ -23,12 +23,7 @@ class Server:
             try:
                 self.__communication_handler.accept_new_connection()
                 bets = self.__communication_handler.recv_msg()
-                store_bets(bets)
-                logging.info(f"action: apuesta_recibida | result: success | cantidad: {len(bets)}")
-                self.__communication_handler.send_batch_success()
-
-            except MessageReceptionError as e:
-                self.__communication_handler.send_batch_failure()
+                self._handle_batch_message(bets)
 
             except Exception as e:
                 logging.error(f"failed to handle client connection. Error: {e}")
@@ -36,3 +31,12 @@ class Server:
             finally:
                 self.__communication_handler.close_current_connection()
 
+    def _handle_batch_message(self, batch: list[Bet]):
+        try:
+            bets = batch
+            store_bets(bets)
+            logging.info(f"action: apuesta_recibida | result: success | cantidad: {len(bets)}")
+            self.__communication_handler.send_batch_success()
+        except Exception as e:
+            logging.error(f"action: apuesta_recibida | result: fail | cantidad: {len(bets)}")
+            self.__communication_handler.send_batch_failure()
